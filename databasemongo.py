d523a319f86cac38a0000845d0ac109148ff0c78
Alexeigithubproject <alexeiprsk@gmail.com>
2025-06-02 07:31:37 +0300

    nginx compose

--- databasemongo.py
+++ databasemongo.py
@@ -4,7 +4,11 @@ from sportsbook.models_mongo import Bet,  Event
 import os
 
 
-MONGO_URI = 'mongodb://mongodb:27017'
+MONGO_URI = os.getenv(
+    'MONGO_URI'
+    ,'mongodb://localhost:27017'
+    )
+
 MONGO_DB_NAME= 'PlayerBets'
 
 async def init_db():
